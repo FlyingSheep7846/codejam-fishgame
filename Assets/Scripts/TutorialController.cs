8bb197cff0f5cbee5f9241905621618f9c44a8ec
FlyingSheep7846 <kevinbai222@gmail.com>
2025-11-16 03:17:55 -0500

    PUSHHHH

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -45,7 +45,7 @@ public class Tutorial : MonoBehaviour
         if (Debounce) return;
         Debounce = true;
 
-        int newSlide = (currentSlide + pages.Count + direction) % pages.Count;
+        int newSlide = 0;
         if (newSlide == currentSlide) return;
 
         pages[currentSlide].DOFade(0f, 0.5f).OnComplete(
